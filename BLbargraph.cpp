41ba13dccf1ddfb8bda129a7c6c5e5e2f0e64730
MordasTheMan <168765157+MordasTheMan@users.noreply.github.com>
2024-05-02 13:18:58 -0400

    Create BLbargraph.cpp

---
+++ BLbargraph.cpp
@@ -0,0 +1,88 @@
+#include <Wire.h>
+#include "BLbargraph.h"
+
+void BG::init(uint8_t addr)
+{  
+  // set the I2C address
+  _i2c_addr = addr;
+  
+  _buffer = (uint16_t*)calloc(8, sizeof(uint16_t));
+  
+  Wire.begin();
+  Wire.beginTransmission(_i2c_addr);
+  Wire.write(0x21); 
+  Wire.write(BB_CMD_SETUP | BB_DISPLAY_ON); 
+  Wire.endTransmission();
+  
+  setBrightness(15);
+  write();
+}
+
+void BargraphLED::setBrightness(uint8_t brightness)
+{
+  brightness = brightness & 0x0F;
+  
+  Wire.beginTransmission(_i2c_addr);
+  Wire.write(BB_CMD_DIMMING | brightness);
+  Wire.endTransmission();
+}
+
+void BargraphLED::clear(void)
+{  
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    _buffer[i] = 0;
+  }  
+}
+
+void BargraphLED::setPixel(uint8_t col, uint8_t row, uint8_t val)
+{
+  // bounds checking
+  col = col & 0x07;
+  row = row & 0x0F;
+  val = val & 0x01;
+  
+  // write the buffer
+  if (val == 1)
+  {
+    _buffer[col] |= 1 << row;
+  }
+  else
+  {
+    _buffer[col] &= ~(1 << row);
+  }
+
+}
+
+void BargraphLED::write(void)
+{
+  Wire.beginTransmission(_i2c_addr);
+  Wire.write(BB_CMD_RAM);
+  
+  for (uint8_t col = 0; col < 8; col++)
+  {
+    writeRow(col);
+  }
+  
+  Wire.endTransmission();
+}
+
+void BargraphLED::setSegment(uint8_t pos, uint8_t val)
+{
+  uint8_t row, col;
+  
+  val &= 0x01;	//constrain val to be either 0 or 1
+  
+  // verify the position isn't greater than the bargraph size
+  if(pos > (BG_SIZE - 1) ) pos = BG_SIZE - 1;
+  
+  row = pos / 4;
+  col = pos % 4;
+	
+  setPixel(col, row, val);
+}
+
+uint8_t BargraphLED::size(void)
+{
+  return BG_SIZE;
+}
