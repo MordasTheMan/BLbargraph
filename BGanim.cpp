bf1e062da6e4971b881974a8bc92106eb8924a6a
MordasTheMan <168765157+MordasTheMan@users.noreply.github.com>
2024-05-03 14:14:50 -0400

    Create BGanim.cpp

---
+++ BGanim.cpp
@@ -0,0 +1,30 @@
+#include "BGanim.h"
+#include "BLbargraph.h"
+BG bargraph = BG();
+
+void BGanim::BBinit()
+{
+  BB.init(0x70);
+  delay(1000);
+  BB.setBrightness(15);
+}
+
+void BGanim::changeInterval(uint8_t NewInterval)
+{
+  IntervalBG = NewInterval;
+}
+
+void BGanim::seqIdle1(unsigned long currentMillis)
+{
+  if ((currentMillis - lastUpdateBG) > IntervalBG)
+  {
+    lastUpdateBG = currentMillis;
+    for (int i = 0; i <= 27; i++)
+    {
+      if (i == IndexSegment )
+        {
+          drawBarGraph(barGraph[i][0], barGraph[i][1], 1);
+        }
+    }
+  }
+}
